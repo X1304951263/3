467b6d2dd254171735d394db3ea9cf1fdfa2c239
1304951263@qq.com <1304951263@qq.com>
2020-10-07 22:21:05 +0800

    实践3
    666

---
+++ app/src/main/java/com/example/getcamera/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.getcamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn=(Button)findViewById(R.id.photo);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, Activity.DEFAULT_KEYS_DIALER);
+            }
+        });
+
+
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView view = (ImageView) findViewById(R.id.view);
+            view.setImageBitmap(imageBitmap);
+        }
+    }
+}
